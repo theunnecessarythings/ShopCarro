1e0c57e6444e647555f489960b2401ac78c0492e
Sai Sruthi <Sruthi@Sais-MacBook-Pro.local>
2018-05-25 19:18:15 +0530

    Add to Cart

--- addToCart/src/main/java/com/coviam/shopcarro/addToCart/controller/AddToCartController.java
+++ addToCart/src/main/java/com/coviam/shopcarro/addToCart/controller/AddToCartController.java
@@ -1,36 +1,57 @@
 package com.coviam.shopcarro.addToCart.controller;
 
 import com.coviam.shopcarro.addToCart.dto.CartDetailsDto;
-import com.coviam.shopcarro.addToCart.dto.DetailsDto;
 import com.coviam.shopcarro.addToCart.services.IAddToCartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+/**
+ *
+ * @author: sandeepgupta
+ *
+ * */
 
 @RestController
 
 public class AddToCartController {
     @Autowired
     private IAddToCartService iAddToCartService;
+
+    /**
+     *
+     * localhost:8080/add-cart
+     *
+     * This will be adding the products in the cart DB, it's a get request and will be returning Boolean value
+     * whether the product can be added to the cart or not.
+     *
+     * */
+
     @RequestMapping(value = "/add-cart", method = RequestMethod.GET)
-    ResponseEntity<Boolean> addCart(@RequestParam String email,String merchantId,String Id) {
-
-        iAddToCartService.addToCart(email,merchantId,Id);
-//
+    ResponseEntity<Boolean> addCart(@RequestParam String email,String merchantId,String id) {
+        iAddToCartService.addToCart(email,merchantId,id);
         return new ResponseEntity<>(true, HttpStatus.CREATED);
-
     }
 
-    @RequestMapping(value = "/get-cart", method = RequestMethod.POST)
-    ResponseEntity<CartDetailsDto> add(@RequestParam String email) {
 
+    /**
+     *
+     * localhost:8080/get-cart
+     *
+     * This function will be returing the cartDetailsDto which is called by the ORDER module to get all the products
+     * for the particular user.
+     *
+     * CartDetailsDto is an object having email, and merchantId and ProductId (id - id where-ever is used is called upon as productId)
+     *
+     *
+     * */
+
+    @RequestMapping(value = "/get-cart", method = RequestMethod.GET)
+    ResponseEntity<CartDetailsDto> get(@RequestParam String email) {
+        System.out.println(email);
         CartDetailsDto cartDetailsDto = iAddToCartService.getCart(email);
-//
         return new ResponseEntity<>(cartDetailsDto, HttpStatus.CREATED);
-
     }
 
 
--- addToCart/src/main/java/com/coviam/shopcarro/addToCart/services/AddToCartImpl.java
+++ addToCart/src/main/java/com/coviam/shopcarro/addToCart/services/AddToCartImpl.java
@@ -1,7 +1,7 @@
 package com.coviam.shopcarro.addToCart.services;
 
 import com.coviam.shopcarro.addToCart.dto.CartDetailsDto;
-import com.coviam.shopcarro.addToCart.dto.DetailsDto;
+import com.coviam.shopcarro.addToCart.dto.Details;
 import com.coviam.shopcarro.addToCart.model.CartDetails;
 import com.coviam.shopcarro.addToCart.repository.IAddToCartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,39 +11,70 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ *
+ * @author: sandeepgupta
+ *
+ * */
+
 @Service
 public class AddToCartImpl implements IAddToCartService {
     @Autowired
     private IAddToCartRepository iAddToCartRepository;
 
+    /**
+     *
+     *  addToCart implementation is used to insert the data in the DB.
+     *
+     * */
+
+
     @Override
     public void addToCart(String email, String merchantId, String Id) {
            // CartDetails cartDetails1 = new CartDetails(email);
+        System.out.println(Id);
+
+        /**
+         *
+         * if the key (email) doesn't exists in the DB than do this or do it otherwise
+         *
+         * */
         if (!iAddToCartRepository.existsById(email)) {
-            List<DetailsDto> detailsDtoList = new ArrayList<>();
-            detailsDtoList.add(new DetailsDto(merchantId,Id));
-            CartDetails cartDetails = new CartDetails(email,detailsDtoList);
+            List<Details> detailsList = new ArrayList<>();
+            System.out.println(Id);
+            detailsList.add(new Details(merchantId,Id));
+            CartDetails cartDetails = new CartDetails(email, detailsList);
             iAddToCartRepository.save(cartDetails);
         }
         else{
             Optional<CartDetails> cartDetailsOpt = iAddToCartRepository.findById(email);
-            if(cartDetailsOpt.isPresent()) {
-                CartDetails cartDetails = cartDetailsOpt.get();
-                List<DetailsDto> list = new ArrayList<>();
-                list = cartDetails.getDetailsDtos();
-                list.add(new DetailsDto(merchantId, Id));
-                CartDetails cart = new CartDetails(email, list);
-                iAddToCartRepository.save(cart);
-            }
+            CartDetails cartDetails = cartDetailsOpt.get();
+            List<Details> list = new ArrayList<>();
+            list = cartDetails.getDetails();
+            list.add(new Details(merchantId, Id));
+            CartDetails cart = new CartDetails(email, list);
+            iAddToCartRepository.save(cart);
         }
     }
 
+
+    /**
+     *
+     * This will be returning the details if the key exists in the DB otherwise returning null.
+     *
+     * */
+
     @Override
     public CartDetailsDto getCart(String email) {
-        Optional<CartDetails> cartDetailsOpt = iAddToCartRepository.findById(email);
-        return new CartDetailsDto(cartDetailsOpt);
-
-
+        //System.out.println(iAddToCartRepository.findAll());;
+        if(iAddToCartRepository.existsById(email)) {
+            Optional<CartDetails> cartDetailsOpt = iAddToCartRepository.findById(email);
+            System.out.println(cartDetailsOpt);
+            return new CartDetailsDto(cartDetailsOpt);
+        }
+        else{
+            return null;
+        }
     }
 
 
